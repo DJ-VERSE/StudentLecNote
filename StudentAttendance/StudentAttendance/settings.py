3bc764e4c40bb6c82a1e7d1319f4dbfe229555ab
DJ-VERSE <dj.codes28@gmail.com>
2025-08-06 23:26:04 +0530

    Update settings.py

--- StudentAttendance/StudentAttendance/settings.py
+++ StudentAttendance/StudentAttendance/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-a)*p=(@(uubic7ts5l*hnv#@8!hq8@mh!o88z#!zi(z)96(mzm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*","https://studentlecnote.onrender.com/"]
+ALLOWED_HOSTS = ["*","studentlecnote.onrender.com"]
 
 
 # Application definition
@@ -171,4 +171,5 @@ print("Cloudinary Check:")
 print("Cloud Name:", CLOUDINARY_STORAGE['CLOUD_NAME'])
 print("API Key:", CLOUDINARY_STORAGE['API_KEY'])
 print("API Secret:", CLOUDINARY_STORAGE['API_SECRET'])
+
 
